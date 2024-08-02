74237364c3ee34723584930536bb291cfb5b817e
Nicholas Araujo <nickferaa@gmail.com>
2024-08-01 21:45:41 -0300

    added logger & all exceptions catcher

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
+import "dotenv/config";
+
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { dbSourceOptions } from "./config/typeorm";
 import { UserModule } from "./user/user.module";
+import { LoggerModule } from "nestjs-pino";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dbSourceOptions), UserModule],
+  imports: [
+    TypeOrmModule.forRoot(dbSourceOptions),
+    LoggerModule.forRoot({
+      pinoHttp:
+        process.env.NODE_ENV === "development"
+          ? {
+              transport: {
+                target: "pino-pretty",
+                options: {
+                  colorize: true,
+                },
+              },
+            }
+          : undefined,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import "dotenv/config";
 
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger } from "nestjs-pino";
+import { AllExceptionsFilter } from "./common/filter/allExceptions.filter";
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
 
   app.setGlobalPrefix("/api/v1");
+
+  app.useLogger(app.get(Logger));
 
   await app.listen(process.env.PORT || 3000);
 }
